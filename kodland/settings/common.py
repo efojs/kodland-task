4087dd5a2f535b9d07eccebcee8dbf333394ee38
Denis <denis@efojs.com>
2020-08-17 17:39:00 +0300

    Add main to installed_apps

--- kodland/settings/common.py
+++ kodland/settings/common.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # Application definition
 
 INSTALLED_APPS = [
+    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
