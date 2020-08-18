38348ac01592cd8f5207f469f6b28c5306c55848
Denis <denis@efojs.com>
2020-08-19 00:17:54 +0300

    Add UI

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Uploaded to AWS S3 using `boto3` and `django-storages`
 
 ### Preview
 Inspired by https://github.com/naritotakizawa/django-preview-image
+
+
+## UI
+
+Some changes were made to the provided UI, because of lack of fonts, unclear constraints for illustrations
--- main/forms.py
+++ main/forms.py
@@ -9,6 +9,10 @@ from .widgets import FileInputWithPreview
 
 
 class BlogPostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update(placeholder='Введите название статьи')
+
     class Meta:
         model = BlogPost
         fields = ['title', 'text', 'image']
